7412e3a5534f0bf5a2662720345a46ed308b10f1
Nathan F Johnson <nathanfjohnson@me.com>
2017-04-20 09:43:31 -0500

    creating framework project (your welcome Carthage)

---
+++ NFJDemoWindow/NFJDemoWindow.h
@@ -0,0 +1,19 @@
+//
+//  NFJDemoWindow.h
+//  NFJDemoWindow
+//
+//  Created by Nathan.F.Johnson on 4/20/17.
+//  Copyright © 2017 NathanFJohnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NFJDemoWindow.
+FOUNDATION_EXPORT double NFJDemoWindowVersionNumber;
+
+//! Project version string for NFJDemoWindow.
+FOUNDATION_EXPORT const unsigned char NFJDemoWindowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NFJDemoWindow/PublicHeader.h>
+
+
